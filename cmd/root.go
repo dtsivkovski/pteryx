cdb5177e5edd85ef7b86180db5cfcf2f82e22ba5
Daniel Tsivkovski <89223402+dtsivkovski@users.noreply.github.com>
2026-04-29 11:44:32 -0700

    implement hardcoded jpeg check

--- cmd/root.go
+++ cmd/root.go
@@ -4,9 +4,16 @@ Copyright © 2026 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
+)
+
+const (
+	cyan = "\033[36m"
+	red   = "\033[31m"
+	reset = "\033[0m"
 )
 
 // rootCmd represents the base command when called without any subcommands
@@ -24,10 +31,10 @@ var rootCmd = &cobra.Command{
       ▓███▓▓▓███▓██▓████████████                        ▓▓  
      ▓█████▓▓████▓▓█████████▓█                           ██ 
     ▓████████████████████▓██                               █
-  ▓█████████▓      ▓████▓█                                  
-  ▓██████           ▓███▓ █         ==============          
- ▓████▓              █▓█ █          ==  PTERYX  ==          
- ███▓▓                 ██ █         ==============          
+  ▓█████████▓      ▓████▓█          ==============          
+  ▓██████           ▓███▓ █         ==  PTERYX  ==          
+ ▓████▓              █▓█ █          ==============          
+ ███▓▓                 ██ █                                 
 ▓███                     ▓                                  
 ███                       █                                 
 ██                                                          
@@ -48,7 +55,33 @@ made for use in digital forensics and incident response and malware analysis.`,
 
 // checks the file signature
 func runFileCheck(filePath string) error {
-	print(filePath)
+	// try to open file
+	f, err := os.Open(filePath)
+	if err != nil {
+		return fmt.Errorf("open %q: %w", filePath, err)
+	}
+	defer f.Close()
+
+	// make 4 byte buffer
+	buffer := make([]byte, 4)
+
+	// read attempt
+	n, err := f.Read(buffer)
+	if err != nil {
+		return fmt.Errorf("read %q: %w", filePath, err)
+	}
+
+	if n < 3 { // file too small
+		return fmt.Errorf("%q is too small to check for a JPEG signature", filePath)
+	}
+
+	// hardcode JPEG check initially
+	if buffer[0] == 0xFF && buffer[1] == 0xD8 && buffer[2] == 0xFF {
+		fmt.Printf("%s✓%s %s %sis a .JPEG%s\n", cyan, reset, filePath, cyan, reset)
+		return nil
+	}
+
+	fmt.Printf("%s✗%s %s %sis not a .JPEG%s\n", red, reset, filePath, red, reset)
 	return nil
 }
 
@@ -70,5 +103,5 @@ func init() {
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
