f8464d36c42ec3836a601dae163a11ca9e50b5c3
Daniel Tsivkovski <89223402+dtsivkovski@users.noreply.github.com>
2026-04-29 13:53:24 -0700

    added signature matching

--- cmd/fileops.go
+++ cmd/fileops.go
@@ -57,9 +57,51 @@ func runFileCheck(filePath string) error {
 
 	if !matched {
 		fmt.Printf("%s✗%s %s %sis not a %s file%s\n", red, reset, filePath, red, ext, reset)
+			actualExts := findSignatureMatches(buffer)
+			if len(actualExts) == 0 {
+				fmt.Print("└─ Unknown signature\n")
+			} else {
+				fmt.Printf("%s└─ File signature matches: %s", red, reset)
+				for i := range actualExts {
+					if i == len(actualExts) - 1 {
+						fmt.Printf("%s\n", actualExts[i])
+					} else {
+						fmt.Printf("%s, ", actualExts[i])
+					}
+				}
+			}
 		return nil
 	}
 
 	fmt.Printf("%s✓%s %s %sis a %s file%s\n", cyan, reset, filePath, cyan, ext, reset)
 	return nil
+}
+
+// check if signature matches anything else 
+func findSignatureMatches(buffer []byte) []string {
+	var matches []string // account for multiple signature possibilities
+
+	for ext, signatures := range fileSignatures {
+		for _, signature := range signatures {
+			if len(buffer) < len(signature) { // if length is shorter, skip
+				continue
+			}
+
+			// match signature
+			matched := true
+			for i, b := range signature {
+				if buffer[i] != b {
+					matched = false
+					break
+				}
+			}
+
+			if matched {
+				matches = append(matches, ext)
+				break
+			}
+		}
+	}
+
+	return matches
 }
